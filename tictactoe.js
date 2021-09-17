0d3630219e2bdfb47f51ad590c9548f056b6750e
Sam <milk3531@hotmail.com>
2021-09-17 12:45:23 -0700

    Initial js and html commit

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,13 @@
 
 // Gameboard will be a module because we only need one gameboard
 const Gameboard = (() => {
-    const gameboard = 
-       ['X', 'O', 'O',
-        'X', 'X', 'O',
-        'O', 'X', 'X']; // This would be an empty board
+    const gameboard =
+        ['X', 'O', 'O',
+            'X', 'X', 'O',
+            'O', 'X', 'X']; // This would be an empty board
+    // Render this onto the website with html
+
+    return { gameboard }; // Return the Gameboard's gameboard for DOM manipulation
 })();
 
 
@@ -16,4 +19,8 @@ const Player = () => {
 // Not sure what this is for yet, but I assume we'll just need one controller for the one board and 2 players
 const displayController = () => {
 
+}
+
+function displayBoard() {
+    // Maybe here receive from Gameboard and display the moves?
 }
