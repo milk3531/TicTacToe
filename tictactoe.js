6e1c9e70234b4fd2e3657f1cf506c00f8c6a3c54
Sam <milk3531@hotmail.com>
2022-01-04 18:11:21 -0800

    logic for ties

--- tictactoe.js
+++ tictactoe.js
@@ -10,9 +10,10 @@ function clickbutton(id) {
             document.getElementById(id).textContent = 'o';
             curcount--;
         }
+        getWinnerX();
+        getWinnerO();
     }
-    getWinnerX();
-   
+
 }
 
 function getGameStatus() {
@@ -20,33 +21,130 @@ function getGameStatus() {
 }
 
 function getWinnerX() {
-    if (document.getElementsByClassName('wrapper')[0].children[0, 1, 2].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[3, 4, 5].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[6, 7, 8].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[0, 4, 8].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[2, 4, 6].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[0, 3, 6].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[1, 4, 7].textContent === 'x' ||
-        document.getElementsByClassName('wrapper')[0].children[2, 5, 8].textContent === 'x') {
-            console.log('X won');
+    if (document.getElementsByClassName('wrapper')[0].children[0].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[1].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[2].textContent === 'x') {
+        console.log('X won');
+        reset();
     }
+
+    if (document.getElementsByClassName('wrapper')[0].children[3].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[5].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[6].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[7].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[8].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[0].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[8].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[2].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[6].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[0].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[3].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[6].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[1].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[7].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[2].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[5].textContent === 'x' &&
+        document.getElementsByClassName('wrapper')[0].children[8].textContent === 'x') {
+        console.log('X won');
+        reset();
+    }
+
 }
 
 function getWinnerO() {
-    if (document.getElementsByClassName('wrapper')[0].children[0, 1, 2].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[3, 4, 5].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[6, 7, 8].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[0, 4, 8].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[2, 4, 6].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[0, 3, 6].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[1, 4, 7].textContent === 'o' ||
-        document.getElementsByClassName('wrapper')[0].children[2, 5, 8].textContent === 'o') {
-            console.log('O won');
+    if (document.getElementsByClassName('wrapper')[0].children[0].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[1].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[2].textContent === 'o') {
+        console.log('O won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[3].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[5].textContent === 'o') {
+        console.log('O won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[6].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[7].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[8].textContent === 'o') {
+        console.log('O won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[0].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[8].textContent === 'o') {
+        console.log('O won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[2].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[6].textContent === 'o') {
+        console.log('O won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[0].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[3].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[6].textContent === 'o') {
+        console.log('O won');
+        reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[1].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[4].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[7].textContent === 'o') {
+        console.log('O won');
+         reset();
+    }
+
+    if (document.getElementsByClassName('wrapper')[0].children[2].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[5].textContent === 'o' &&
+        document.getElementsByClassName('wrapper')[0].children[8].textContent === 'o') {
+        console.log('O won');
+        reset();
     }
 }
 
 function getTie() {
-    if (document.getElementsByClassName('wrapper')[0].children[0,1,2,3,4,5,6,7,8].textContent !== ' ') {
+    if (document.getElementsByClassName('wrapper')[0].children[0, 1, 2, 3, 4, 5, 6, 7, 8].textContent !== ' ') {
         console.log('Game is a tie');
+    }
+}
+
+function reset() {
+    for (let i = 0; i < 9; i++) {
+        document.getElementsByClassName('wrapper')[0].children[i].textContent = ' ';
     }
 }
